7c5290f4de5dd452e25548a91c4b8099dfc618b3
Pyrel1ght <149167248+Pyrel1ght@users.noreply.github.com>
2023-12-18 21:26:00 +0300

    Много чего
    Обновил модель и текстуру оружия
    Обновил уровень и его текстуры
    Добавил эффекты при выстреле и эффекты на поверхности куда попала пуля

---
+++ Курсач/Assets/Scripts/Pickup.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    // Start is called before the first frame update
+    bool Inventory = false;
+    public GameObject GunPrefab;
+    public GameObject Player;
+    public GameObject Camera;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Weapon" && !Inventory)
+        {
+            Destroy(other.gameObject);
+            pickUp();
+            
+        }
+    }
+    private void pickUp()
+    {
+        Inventory = true;
+        Instantiate(GunPrefab, new Vector3(0.638f, -0.26f, 0.716f), Quaternion.identity);
+        GameObject Gun = GameObject.Find("Gun(Clone)");
+        Gun.transform.SetParent(Camera.transform, false);
+        Gun.transform.localEulerAngles = new Vector3(0.393f, -73.481f, -3.819f);
+    }
+}
